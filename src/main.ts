b5b1325c44f15de8caa9df3b5ec8781ee9a3d907
gnietto <elcirculoti@gmail.com>
2022-06-28 12:05:03 -0400

    Corrige imports, ruta raíz y puerto de servidor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import fastify, { FastifyInstance } from 'fastify';
+import { fastify, FastifyInstance } from 'fastify';
 
 const server: FastifyInstance = fastify({ logger: true });
 
-server.get('/', function (reply: any) {
-  reply.send({ hello: 'world' });
+server.get('/', function (request: any, reply: any) {
+  reply.send('Primer mensaje');
 });
 
-server.listen({ port: 3000 }, function (err: any) {
+server.listen({ port: 3002 }, function (err: any) {
   if (err) {
     server.log.error(err);
     process.exit(1);
