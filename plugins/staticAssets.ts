7a52992bc2f7ee961d04470906663b08d8714592
gnietto <elcirculoti@gmail.com>
2022-07-02 11:58:41 -0400

    Agrega uso de static assets al servidor web; corrige sintaxis

---
+++ plugins/staticAssets.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fastifyStatic from '@fastify/static';
+import fp from 'fastify-plugin';
+
+// workaround para disponibilizar __filename y __dirname en ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const staticAssets = fp(async function (fastify: any) {
+  fastify.register(fastifyStatic, {
+    root: path.join(__dirname, '../public'),
+    prefix: '/public/'
+  });
+});
+
+export default staticAssets;
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 import { fastify, FastifyInstance } from 'fastify';
+import staticAssets from '../plugins/staticAssets.js';
 
+const PORT: number = 3002;
 const f: FastifyInstance = fastify({ logger: true });
+
+f.register(staticAssets);
 
 f.get('/', function (request: any, reply: any) {
   console.log(request.body);
-  reply.send('Primer mensaje desplegado en ruta raíz');
+  return reply.sendFile('test.html');
 });
 
 f.get('/test', function (request: any, reply: any) {
   console.log(request.body);
-  reply.send('Mensaje desplegado en la ruta raíz slash test');
+  return reply.send('Mensaje desplegado en la ruta /test');
 });
 
-f.listen(function (err: any) {
+f.listen({ port: PORT }, function (err: any, address: any) {
   if (err) {
     f.log.error(err);
     process.exit(1);
   }
+  console.log(`Listening on ${address}`);
 });
