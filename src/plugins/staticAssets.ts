fb9717f7c49c4f325703a143adc324f147d09c81
gnietto <elcirculoti@gmail.com>
2022-07-03 22:39:13 -0400

    [fix] Agrega comentarios de typescript temporalmente

--- src/plugins/staticAssets.ts
+++ src/plugins/staticAssets.ts
@@ -1,9 +1,10 @@
-import path from 'path';
+import * as path from 'path';
 import { fileURLToPath } from 'url';
 import fastifyStatic from '@fastify/static';
 import fp from 'fastify-plugin';
 
 // workaround para disponibilizar __filename y __dirname en ESM
+// @ts-ignore
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
